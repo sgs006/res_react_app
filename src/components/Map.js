276e88ff8808c8671b3e63e81d6457d18c121e2c
Sam Stokes <sgstokes006@gmail.com>
2018-11-12 19:05:40 -0600

    git upload

---
+++ src/components/Map.js
@@ -0,0 +1,72 @@
+/* global google */
+import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={8}
+      zoom={props.zoom}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      center={props.center}
+    >
+      {props.markers &&
+        props.markers
+          .filter(marker => marker.isVisible)
+          .map((marker, idx, arr) => {
+            const venueInfo = props.venues.find(
+              venue => venue.id === marker.id
+            );
+            return (
+              <Marker
+                key={idx}
+                position={{ lat: marker.lat, lng: marker.lng }}
+                onClick={() => props.handleMarkerClick(marker)}
+                animation={
+                  arr.length === 1
+                    ? google.maps.Animation.BOUNCE
+                    : google.maps.Animation.DROP
+                }
+              >
+                {marker.isOpen &&
+                  venueInfo.bestPhoto && (
+                    <InfoWindow>
+                      <React.Fragment>
+                        <img
+                          src={`$(venueInfo.bestPhoto.prefix}200x200${
+                            venueInfo.bestPhoto.suffix
+                          }`}
+                          alt={"Venue image"}
+                        />
+                        <p>venueInfo.name</p>
+                      </React.Fragment>
+                    </InfoWindow>
+                  )}
+              </Marker>
+            );
+          })}
+    </GoogleMap>
+  ))
+);
+
+class Map extends Component {
+  render() {
+    return (
+      <MyMapComponent
+        {...this.props}
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAbEzNEjUz1RmFTDQNMyRcuJHyxGWkoAqY"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%`, width: `75%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
+  }
+}
+
+export default Map;
